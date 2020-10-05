5a4daa2745f69e6bda4a5dea189ed3cea30b2b10
Getulio Moraes <getulio.vieira@hp.com>
2020-10-04 21:58:22 -0300

    Only saving users when needed

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
 import { downloadUsers } from "../helpers/downloadUsers";
 import { filterUsers } from "../helpers/filterUsers";
 import { storeUsers } from "../helpers/storeUsers";
 import { UserType } from "../types";
+import { User } from "../entity/User";
 
 let downloadedUsers: Array<UserType> = [];
 
@@ -21,10 +23,21 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const saveUsers = async (req: Request, res: Response) => {
   try {
-    const filteredUsers = filterUsers(downloadedUsers);
-    await storeUsers(filteredUsers);
-    res.status(201).json(filteredUsers);
+    const storedUsers = await getRepository(User).find();
+
+    if (Object.values(storedUsers).length === 0) {
+      const filteredUsers = filterUsers(downloadedUsers);
+      await storeUsers(filteredUsers);
+      res.status(201).json(filteredUsers);
+    } else {
+      const filteredUsers = filterUsers(downloadedUsers);
+      res.status(304).json({
+        message: "You Already Saved your Users!",
+        data: filteredUsers,
+      });
+    }
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: "Internal Server Error",
       error: error,
