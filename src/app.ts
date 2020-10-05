4ababbc3cabe8dde326a8208e756cc6237d6241c
Getulio Moraes <getulio.vieira@hp.com>
2020-10-04 21:05:57 -0300

    Initializing a Conection with mySQL database using TypeORM

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Application } from "express";
 import cors from "cors";
 import routes from "./routes";
+import { createConnection } from "typeorm";
 
-const app = express() as Application;
+createConnection()
+  .then(async (connection) => {
+    const app = express() as Application;
 
-app.use(cors());
+    app.use(cors());
 
-app.use(routes);
+    app.use(routes);
 
-app.set("json spaces", 4);
+    app.set("json spaces", 4);
 
-app.listen(8080);
+    app.listen(8080);
+  })
+  .catch((error) => console.log(error));
